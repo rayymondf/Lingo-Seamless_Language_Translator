6d35424848fc9fb4ffa96f2d5ced0bc9c28ecc71
Raymond Fang <rayymondf29@gmail.com>
2026-04-09 11:32:20 -0400

    Implement language loading in user interface
    Fetches available target languages from DeepL API and populates a dropdown.

---
+++ userInterface.js
@@ -0,0 +1,29 @@
+
+const targetLanguage = document.getElementById("targetLanguage")
+async function loadLanguages() {
+    const response = await fetch("https://api-free.deepl.com/v2/languages?type=target", {
+        method: "GET",
+        headers: {
+            "Authorization": "DeepL-Auth-Key 0b7f2edf-519b-4fc1-b0ea-1d694c61180a:fx"
+        }
+    })
+
+    const languages = await response.json()
+
+    //console.log(languages)
+
+    for(let i = 0; i < languages.length; i++){
+        //console.log(languages[i].name + " " + languages[i].language)
+        let option = document.createElement("option")
+        option.value = languages[i].language 
+        option.textContent = languages[i].name
+
+        if (languages[i].language == "ZH"){
+            option.textContent = "Chinese (generic)"
+
+        }
+        targetLanguage.append(option)
+
+    }
+}
+loadLanguages()
